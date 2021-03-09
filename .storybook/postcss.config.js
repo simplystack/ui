a12d77a0613c7f2c8db1549f561266b3c01574ce
Roman Slonov <destabiliseus@gmail.com>
2021-03-09 17:58:19 +0800

    Delete storybook

--- .storybook/postcss.config.js
+++
@@ -1,8 +0,0 @@
-const tailwindcss = require('tailwindcss');
-
-module.exports = {
-  plugins: [
-    tailwindcss('tailwind.config.js'), //This refers to your tailwind config
-    require('autoprefixer'),
-  ],
-};
--- .storybook/preview.js
+++
@@ -1,5 +0,0 @@
-import './main.css';
-
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
