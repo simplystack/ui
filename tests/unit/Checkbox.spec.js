4e97d9286cd5b6874e10e594efb6c3c5691d623e
Roman Slonov <destabiliseus@gmail.com>
2021-05-13 15:58:28 +0800

    Updated checkbox unit test

--- tests/unit/Checkbox.spec.js
+++ tests/unit/Checkbox.spec.js
@@ -7,10 +7,62 @@ describe('Checkbox.vue', () => {
     const wrapper = mount(Checkbox, {
       props: {
         modelValue: false,
+        label: 'My label',
       },
     });
 
-    expect(wrapper.find('label').exists()).toBe(true);
+    const label = wrapper.find('label');
+
+    expect(label.exists()).toBe(true);
+    expect(label.text()).toBe('My label');
     expect(wrapper.find('input').exists()).toBe(true);
+  });
+
+  it('should emit "change" event correctly', async () => {
+    const wrapper = mount(Checkbox, {
+      props: {
+        modelValue: false,
+      },
+    });
+
+    const input = wrapper.find('input');
+
+    await input.trigger('click');
+
+    expect(wrapper.emitted('change')).toBeDefined();
+    expect(wrapper.emitted('change').length).toBe(1);
+    expect(wrapper.emitted('change')[0][0]).toBe(true);
+    expect(input.element.checked).toBe(true);
+
+    await input.trigger('click');
+
+    expect(wrapper.emitted('change')).toBeDefined();
+    expect(wrapper.emitted('change').length).toBe(2);
+    expect(wrapper.emitted('change')[1][0]).toBe(false);
+    expect(input.element.checked).toBe(false);
+  });
+
+  it('should emit "update:modelValue" event correctly', async () => {
+    const wrapper = mount(Checkbox, {
+      props: {
+        modelValue: false,
+      },
+    });
+
+    const input = wrapper.find('input');
+
+    await input.trigger('click');
+
+    expect(wrapper.emitted('update:modelValue')).toBeDefined();
+    expect(wrapper.emitted('update:modelValue').length).toBe(1);
+    expect(wrapper.emitted('update:modelValue')[0][0]).toBe(true);
+    expect(input.element.checked).toBe(true);
+
+    await input.trigger('click');
+
+    expect(wrapper.emitted('update:modelValue')).toBeDefined();
+    expect(wrapper.emitted('update:modelValue').length).toBe(2);
+    expect(wrapper.emitted('update:modelValue')[1][0]).toBe(false);
+    expect(input.element.checked).toBe(false);
   });
 });
