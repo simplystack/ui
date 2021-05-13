2e963355aaa49d11837eed89cd1f091e90e9e9e2
Roman Slonov <destabiliseus@gmail.com>
2021-05-13 15:59:16 +0800

    Added checkbox group unit test

---
+++ tests/unit/CheckboxGroup.spec.js
@@ -0,0 +1,52 @@
+import { mount } from '@vue/test-utils';
+// eslint-disable-next-line import/no-unresolved
+import CheckboxGroup from '@/components/CheckboxGroup/CheckboxGroup.vue';
+
+const options = [
+  {
+    id: '1', name: 'test', value: 'box-1', label: 'Box 1',
+  },
+  {
+    id: '2', name: 'test', value: 'box-2', label: 'Box 2',
+  },
+  {
+    id: '3', name: 'test', value: 'box-3', label: 'Box 3',
+  },
+  {
+    id: '4', name: 'test', value: 'box-4', label: 'Box 4',
+  },
+];
+
+describe('CheckboxGroup.vue', () => {
+  it('should render checkbox group', () => {
+    const wrapper = mount(CheckboxGroup, {
+      props: {
+        modelValue: [],
+        options,
+        label: 'My label',
+      },
+    });
+
+    expect(wrapper.findAll('label').length).toBe(options.length);
+    expect(wrapper.findAll('input').length).toBe(options.length);
+  });
+
+  it('should render emit "update:modelValue" correctly', async () => {
+    const wrapper = mount(CheckboxGroup, {
+      props: {
+        modelValue: [],
+        options,
+        label: 'My label',
+      },
+    });
+
+    const inputs = wrapper.findAll('input');
+
+    await inputs[0].trigger('click');
+    expect(wrapper.emitted('update:modelValue')[0][0]).toEqual(['box-1']);
+
+    await inputs[2].trigger('click');
+
+    expect(wrapper.emitted('update:modelValue')[1][0]).toEqual(['box-3']);
+  });
+});
