fd506b14b49276d85e0b69a13eb6a6af5ed457b9
Roman <destabiliseus@gmail.com>
2021-05-14 22:38:47 +0800

    Added menu option unit test

---
+++ tests/unit/MenuOption.spec.js
@@ -0,0 +1,32 @@
+import { mount } from '@vue/test-utils';
+// eslint-disable-next-line import/no-unresolved
+import MenuOption from '@/components/MenuOption/MenuOption.vue';
+
+describe('Menu.vue', () => {
+  it('should render menu option', async () => {
+    const wrapper = mount(MenuOption, {
+      props: {
+        label: 'Option 1',
+      },
+    });
+
+    const li = wrapper.find('li');
+
+    expect(li.text()).toBe('Option 1');
+    expect(li.attributes('tabindex')).toBe('0');
+  });
+
+  it('should render disabled menu option', async () => {
+    const wrapper = mount(MenuOption, {
+      props: {
+        label: 'Option 1',
+        disabled: true,
+      },
+    });
+
+    const li = wrapper.find('li');
+
+    expect(li.text()).toBe('Option 1');
+    expect(li.attributes('tabindex')).toBeUndefined(); // meaning option is disabled
+  });
+});
